b3a68c82d671542931a0f7f70cd4d16ee3de462b
Alfonso <alfof87@gmail.com>
2020-10-05 14:59:53 +0200

    laravel-auth

---
+++ MyLaravel2/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'EmployeeController@index') -> name('emp.index');
+Route::get('/show/{id}', 'EmployeeController@show') -> name('emp.show');
+Route::get('/create', 'EmployeeController@create') -> name('emp.create');
+Route::post('/store', 'EmployeeController@store') -> name('emp.store');
+Route::get('/destroy/{id}', 'EmployeeController@destroy') -> name('emp.destroy');
+
+Auth::routes();
