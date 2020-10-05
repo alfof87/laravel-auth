8c1b1c258fa3588b549318201f2ec65e9d82737a
Alfonso <alfof87@gmail.com>
2020-10-05 16:26:09 +0200

    laravel-auth

--- MyLaravel2/resources/views/show.blade.php
+++ MyLaravel2/resources/views/show.blade.php
@@ -1,20 +1,30 @@
 @extends('layouts.app')
 
-@section('section')
-  <div class="flex">
-    <ul>
-        <li>
-          <img src="https://brighterwriting.com/wp-content/uploads/icon-user-default.png" alt="">
-        </li>
-        <li>
-          <h4>Date of Birth:</h4>
-          {{ $emp -> date_of_birth }}
-        </li>
-        <br>
-        <li>
-          <h4>Private Code:</h4>
-          {{ $emp -> private_code }}
-        </li>
-    </ul>
-  </div>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Employees:</div>
+
+                <div class="card-body">
+                  <ul>
+                      <li>
+                        <img style="width: 90px;height: 100px;" src="https://brighterwriting.com/wp-content/uploads/icon-user-default.png" alt="">
+                      </li>
+                      <li>
+                        <h4>Date of Birth:</h4>
+                        {{ $emp -> date_of_birth }}
+                      </li>
+                      <br>
+                      <li>
+                        <h4>Private Code:</h4>
+                        {{ $emp -> private_code }}
+                      </li>
+                  </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
